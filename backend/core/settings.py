cfbbdb930c592d27b6af67aa8bbd1fb17b1ea412
ridhwankhan <ridhwankhan03@gmail.com>
2026-07-16 00:39:16 +0600

    Support PostgreSQL database using dj-database-url

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -71,11 +71,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True if os.getenv('DATABASE_URL') else False
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
